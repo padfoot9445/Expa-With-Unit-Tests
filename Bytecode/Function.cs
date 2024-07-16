112f7799e3ac1f989298c5e7620ff64500fd0d73
unknown <yipy214@gmail.com>
2024-07-16 01:17:19 +0100

    Split Function record into own file

---
+++ Bytecode/Function.cs
@@ -0,0 +1,18 @@
+namespace Bytecode;
+public partial class FunctionProvider
+{
+    record Function : IFunction
+    {
+        public IClassSigniture? ReturnType { get; init; }
+        public IClassSigniture[] ArgTypes { get; init; }
+        public uint[] InversedDefaultArgValuePointers { get; init; }
+        public uint FunctionPointer { get; init; }
+        public Function(IClassSigniture? rt, IClassSigniture[] at, uint[] idavp, uint fp)
+        {
+            this.ReturnType = rt;
+            this.ArgTypes = at;
+            this.InversedDefaultArgValuePointers = idavp;
+            this.FunctionPointer = fp;
+        }
+    }
+}
--- Bytecode/FunctionProvider.cs
+++ Bytecode/FunctionProvider.cs
@@ -1,24 +1,10 @@
 namespace Bytecode;
-public class FunctionProvider : IFunctionProvider
+public partial class FunctionProvider : IFunctionProvider
 {
     private static UniquePointerAndStorageProvider<ISection> BackingList = new();
     ISection[] IFunctionProvider.GetFunctionsArray => GetFunctionsArray;
     public static ISection[] GetFunctionsArray => BackingList.Array;
     private static uint NextFunctionPointer = 0;
-    record Function : IFunction
-    {
-        public IClassSigniture? ReturnType { get; init; }
-        public IClassSigniture[] ArgTypes { get; init; }
-        public uint[] InversedDefaultArgValuePointers { get; init; }
-        public uint FunctionPointer { get; init; }
-        public Function(IClassSigniture? rt, IClassSigniture[] at, uint[] idavp, uint fp)
-        {
-            this.ReturnType = rt;
-            this.ArgTypes = at;
-            this.InversedDefaultArgValuePointers = idavp;
-            this.FunctionPointer = fp;
-        }
-    }
     IFunction IFunctionProvider.New(Bytecode.IClassSigniture? ReturnType, Bytecode.IClassSigniture[] ArgTypes, uint[] InversedDefaultValues, Bytecode.ISection FunctionBody) => New(ReturnType, ArgTypes, InversedDefaultValues, FunctionBody);
     public static IFunction New(IClassSigniture? ReturnType, IClassSigniture[] ArgTypes, uint[] InversedDefaultValues, ISection FunctionBody)
     {
