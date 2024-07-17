8c055bbed7266dfa36a084513d9fd68a113eabe6
unknown <yipy214@gmail.com>
2024-07-17 14:37:58 +0100

    Temporarily implemented interface

--- Bytecode/ClassSigniture.cs
+++ Bytecode/ClassSigniture.cs
@@ -12,4 +12,10 @@ namespace Bytecode;
 public record ClassSigniture() : IClassSigniture
 {
     public int ID => throw new NotImplementedException();
+
+    public IFunction[] NewFunctions => throw new NotImplementedException();
+
+    public IField[] NewFields => throw new NotImplementedException();
+
+    public IClassSigniture[][] Ancestors => throw new NotImplementedException();
 }
