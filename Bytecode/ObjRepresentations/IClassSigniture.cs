0dfbe0441589a185530755a1ef8698ccc4945d5b
unknown <yipy214@gmail.com>
2024-07-18 18:53:45 +0100

    Removed inaccurate comment wrt serialized form

--- Bytecode/ObjRepresentations/IClassSigniture.cs
+++ Bytecode/ObjRepresentations/IClassSigniture.cs
@@ -1,10 +1,6 @@
 namespace Bytecode.ObjRepresentations;
 /// <summary>
 /// Records the class signature for any given class. This has sections for parent classes and interfaces, such that any function call is passed through this object and redirected to the apporopiate function pointer.
-/// For instance, where Class A and B have functions Foo and Bar, and class C overrides Bar, and defines F
-/// Class A | METHOD (void Foo ARG) PTR 1
-/// Class B | METHOD PTR 2
-/// Class C | METHOD PTR 3 || PARENT A | METHOD void FOO ARG PTR 1 || PARENT B | METHOD void FOO ARG PTR 4
 /// </summary> <summary>
 /// 
 /// </summary>
