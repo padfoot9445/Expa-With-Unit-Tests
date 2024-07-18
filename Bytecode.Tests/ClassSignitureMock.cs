d9ce39587e41c8790865fef2ffe942c6cf851705
unknown <yipy214@gmail.com>
2024-07-18 16:13:40 +0100

    Fixed usings

--- Bytecode.Tests/ClassSignitureMock.cs
+++ Bytecode.Tests/ClassSignitureMock.cs
@@ -1,5 +1,12 @@
 namespace Bytecode.Tests.Mocks;
+using Bytecode.ObjRepresentations;
 class ClassSignitureMock : IClassSigniture
 {
     public int ID => throw new NotImplementedException();
+
+    public IFunction[] NewFunctions => throw new NotImplementedException();
+
+    public IField[] NewFields => throw new NotImplementedException();
+
+    public IClassSigniture[][] Ancestors => throw new NotImplementedException();
 }
